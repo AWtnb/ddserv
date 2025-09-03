ce5c7e8433bc4c8dcebf9b2b1554d5f887e58b45
awtnb <arikaebanataw@gmail.com>
2025-09-03 13:35:57 +0900

    load from github

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Markdown to html converter.
 > .\m2h.exe -h
 Usage of C:\Users\awtnb\Sync\develop\repo\m2h\m2h.exe:
   -plain
-        flag to skip loading https://cdn.jsdelivr.net/gh/Awtnb/md-less@latest/style.less
+        flag to skip loading https://raw.githubusercontent.com/AWtnb/md-less/refs/heads/main/style.less
   -src string
         markdown path
   -suffix string
--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 	)
 	flag.StringVar(&src, "src", "", "markdown path")
 	flag.StringVar(&suffix, "suffix", "", "suffix of result html")
-	flag.BoolVar(&plain, "plain", false, "flag to skip loading https://cdn.jsdelivr.net/gh/Awtnb/md-less@latest/style.less")
+	flag.BoolVar(&plain, "plain", false, "flag to skip loading https://raw.githubusercontent.com/AWtnb/md-less/refs/heads/main/style.less")
 	flag.Parse()
 
 	if !strings.HasSuffix(src, ".md") {
